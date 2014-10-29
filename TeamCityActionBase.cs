bbe436a82b0afc912c7c4d63fab025c97a44a6f3
kharnagy <kharnagy@inedo.com>
2014-10-29 11:34:32 -0400

    Fix #10

--- TeamCityActionBase.cs
+++ TeamCityActionBase.cs
@@ -1,5 +1,5 @@
-﻿using System.Net;
-using Inedo.BuildMaster;
+﻿using System;
+using System.Net;
 using Inedo.BuildMaster.Extensibility.Actions;
 
 namespace Inedo.BuildMasterExtensions.TeamCity
@@ -20,12 +20,17 @@ namespace Inedo.BuildMasterExtensions.TeamCity
         {
             var configurer = GetExtensionConfigurer();
 
-            var client = new WebClient()
-            {
-                BaseAddress = configurer.BaseUrl
-            };
+            var client = new WebClient() { BaseAddress = configurer.BaseUrl };
+            
             if (!string.IsNullOrEmpty(configurer.Username))
-                client.Credentials = new NetworkCredential(configurer.Username, configurer.Password);
+            {
+                // Using a CredentialCache because API URLs with TeamCity variables in them will issue redirects
+                // to the actual URLs, and unlike the NetworkCredential class, CredentialCache will ensure that the
+                // credentials will be sent to the redirected URL as well
+                var credentials = new CredentialCache();
+                credentials.Add(new Uri(configurer.BaseUrl), "Basic", new NetworkCredential(configurer.Username, configurer.Password));
+                client.Credentials = credentials;
+            }            
 
             return client;
         }
